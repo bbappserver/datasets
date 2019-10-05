4fb42ee66dea63642326749faf1d6bb3a07658d8
bbserve <bbappserver2@gmail.com>
2019-10-04 17:30:54 -0700

    Fixed windows newlines?

--- scripts/yiff2siblings.py
+++ scripts/yiff2siblings.py
@@ -2,6 +2,7 @@
 import json
 import sys
 import argparse
+import os
 from urllib.request import urlopen,Request
 
 DEFAULT_URL="https://yiff.party/json/creators.json?_=1570229034607"
@@ -10,10 +11,18 @@ parser = argparse.ArgumentParser(description='Convert yiff manifest to hydrus si
 
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-o", "--outfile",help="Path of file to write results",default=None)
-group.add_argument("-p", "--print", help="Printto standard out", action="store_true",default=False)
+group.add_argument("-p", "--print", help="Print to standard out", action="store_true",default=False)
+
+parser.add_argument("-l", "--linesep", help="Override newline for the output file.",default=None)
+
 parser.add_argument("url",nargs="?",default=DEFAULT_URL,help="The url/path of the yiff.party manifest")
 args=parser.parse_args()
 
+if args.linesep is None:
+    linesep=os.linesep if os.name=='nt' else "\r\n"
+else:
+    #format() convert print format characters to actual characters
+    linesep=args.linesep.format()
 url=args.url
 file_out = args.outfile is not None
 req = Request(url, headers=hdr)
@@ -25,10 +34,10 @@ with urlopen(req) as f:
         for u in d["creators"]:
             # yiff id:13538527
             # creator:000
-            s="yiff id:%s\r\ncreator:%s" % (u["id"], u["name"])
+            s="yiff id:%s%screator:%s" % (u["id"],linesep, u["name"])
             if file_out:
                 out.write(s)
-                out.write('\r\n')
+                out.write(linesep)
             else:
                 print(s)
         if file_out:
